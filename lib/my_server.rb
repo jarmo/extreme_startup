fabada7a07319b864d8badae1878b330f4130595
Andri Möll <andri@dot.ee>
2011-09-27 23:21:19 +0300

    Add fallback.

--- lib/my_server.rb
+++ lib/my_server.rb
@@ -4,9 +4,6 @@ require 'sinatra'
 set :port, 1337
 
 get '/' do
-  #192.168.0.118 - - [27/Sep/2011 21:54:24] "GET /?q=0a714870:%20which%20of%20the%20following%20numbers%20is%20both%20a%20square%20and%20a%20cube:%2046656,%20843 HTTP/1.1" 200 89 0.0010
-  #192.168.0.118 - - [27/Sep/2011 21:54:34] "GET /?q=10b8d1b0:%20what%20is%2014%20multiplied%20by%209 HTTP/1.1" 200 45 0.0011
-  
   case params["q"]
   when /what is your name/
     "Jarmo & Andri (Awesome!)"
@@ -21,6 +18,22 @@ get '/' do
   when /what is (\d+) multiplied by (\d+)/
     ($1.to_i * $2.to_i).to_s
   else
-    params.inspect
+    require 'net/http'
+    require 'uri'
+    
+    answer = ""
+    urls = %w[
+      http://192.168.0.123:12345
+      http://195.222.1.250:9000 
+      http://192.168.0.124
+      http://192.168.0.147
+    ].each do |url|
+      uri = URI.parse(URI.encode(url + "/?q=" + params["q"]))
+      answer = Net::HTTP.get(uri)
+      break unless answer.strip.empty?
+    end
+    
+    p answer
+    answer
   end
 end
