f605d57c5feeeb7b9163bde8905eae6bdff688bc
Andri Möll <andri@dot.ee>
2011-09-27 21:36:06 +0300

    Answer more numerical questions.

--- lib/my_server.rb
+++ lib/my_server.rb
@@ -4,5 +4,12 @@ require 'sinatra'
 set :port, 1337
 
 get '/' do
-  "Jarmo & Andri (Awesome!)"
+  case params["q"]
+  when /what is your name/
+    "Jarmo & Andri (Awesome!)"
+  when /what is (\d+) plus (\d+)/
+    ($1.to_i + $2.to_i).to_s
+  else
+    params.inspect
+  end
 end
